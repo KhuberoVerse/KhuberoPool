8afbbe9b3cc3f4c344ab40c481a420d32ff0c772
Abhishek Sinha <avishkarabhishek786@gmail.com>
2022-05-26 22:18:22 +0530

    added merkle

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+//import "@openzeppelin/contracts/access/Ownable.sol";
 import "./KhuberoToken.sol";
 
-contract Pool {
+contract Pool is Ownable {
 
     uint256 public constant WAD = 1e18;
     uint256 public investmentCap;
@@ -11,6 +12,7 @@ contract Pool {
     uint256 public exchangeRate;
     uint256 public minInvestment;
     uint256 public feePercentage;
+    address[] public investorsList;
 
     address public Treasury;
     KhuberoToken public KBRContract;
@@ -29,6 +31,9 @@ contract Pool {
     }
 
     event Received(address, uint);
+    event FeeRecieved(uint256 fee, uint256 datetime);
+    event KBRReceived(address investor, uint256 kbr, uint256 datetime);
+    event EthInvestement(address investor, uint256 eth, uint256 datetime);
     receive() external payable {
         emit Received(msg.sender, msg.value);
     }
@@ -40,23 +45,28 @@ contract Pool {
         return ethSent/ exchangeRate;
     }
 
-    function deductPlatformFee(uint256 inputKBR) internal returns (uint256 outputKBR) {
-        uint256 fee = inputKBR*feePercentage;
+    function getPlatformFee(uint256 inputKBR) internal view returns (uint256 outputKBR, uint256 fee) {
+        fee = inputKBR*feePercentage;
         outputKBR = inputKBR - fee;
         require(outputKBR<=inputKBR, "invalid output KBR");
-        // KBRContract.mint(Treasury, fee);
-        // KBRContract.mint(Treasury, fee);
     }
 
-    function invest() external payable {
+    function invest(bytes32[] memory _merkleProof) external payable {
         require(msg.value>minInvestment, "Below Min Investment.");
         require(investmentTub+msg.value<investmentCap, "Investment overflow");
         uint256 mintedKBR = ethToKBR(msg.value);
 
-        uint256 outputKBR = deductPlatformFee(mintedKBR);
+        (uint256 outputKBR, uint256 fee) = getPlatformFee(mintedKBR);
+
+        KBRContract.mint(msg.sender, outputKBR, _merkleProof);
+        KBRContract.mint(Treasury, fee, _merkleProof);
 
         ethInvestments[msg.sender] += msg.value;
         KBRBalance[msg.sender] += outputKBR;
+        
+        emit KBRReceived(msg.sender, outputKBR, block.timestamp);
+        emit EthInvestement(msg.sender, msg.value, block.timestamp);
+        emit FeeRecieved(fee, block.timestamp);
     }
 
 }
