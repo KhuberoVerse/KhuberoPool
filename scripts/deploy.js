5d977bdfc8d8c005352f6a35308798aa97f98c1b
Abhishek Sinha <avishkarabhishek786@gmail.com>
2022-07-10 20:55:40 +0530

    code to deploy staking

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,6 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
   const Treasury = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199";
   const investmentCap = "10000000000000000000";
   const exchangerate = "1000000000";
@@ -31,7 +19,16 @@ async function main() {
 
   await khuberoToken.deployed();
 
-  console.log("KhuberoToken deployed to:", khuberoToken.address);
+  const KBRAddr = khuberoToken.address;
+
+  console.log("KhuberoToken deployed to:", KBRAddr);
+
+  const Staking = await hre.ethers.getContractFactory("Staking");
+  const staking = await Staking.deploy(KBRAddr);
+
+  await staking.deployed();
+
+  console.log("Staking deployed to:", staking.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
